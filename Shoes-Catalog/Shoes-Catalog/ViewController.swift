8f4fd0240f665427479bd20205a8442de071b266
Taras-Veremchuk <taras.veremm@gmail.com>
2025-02-24 23:05:54 +0100

    Initial project

---
+++ Shoes-Catalog/Shoes-Catalog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shoes-Catalog
+//
+//  Created by Taras Veremchuk on 24.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
